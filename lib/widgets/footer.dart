31bb3473d412ae11f775c34138731d05db4c809e
jiyuujin <jiyuujinunite@gmail.com>
2021-08-05 22:09:29 +0900

    delete unused package

--- lib/widgets/footer.dart
+++ lib/widgets/footer.dart
@@ -2,7 +2,6 @@ import 'package:confwebsite2021/utils/responsive_layout.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class LinkModel {
   LinkModel(this.name, this.url);
