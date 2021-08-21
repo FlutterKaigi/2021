61849f35c931d72761353a1a53b7cd632504792b
Katsumi Onishi <granoeste@gmail.com>
2021-08-21 13:23:34 +0900

    update: staff data

--- lib/data/staff.dart
+++ lib/data/staff.dart
@@ -1,55 +1,6 @@
 final kStaffList = [
   {
-    'name': 'katsummy1',
-    // 'photo': Assets.photo.katsummy.path,
-    'photo':
-        'https://pbs.twimg.com/profile_images/378800000593680476/aa0280b76541ed89ca802472ea174c57_400x400.png',
-    'url': 'https://twitter.com/katsummy',
-  },
-  {
-    'name': 'katsummy2',
-    // 'photo': Assets.photo.katsummy.path,
-    'photo':
-        'https://pbs.twimg.com/profile_images/378800000593680476/aa0280b76541ed89ca802472ea174c57_400x400.png',
-    'url': 'https://twitter.com/katsummy',
-  },
-  {
-    'name': 'katsummy3',
-    // 'photo': Assets.photo.katsummy.path,
-    'photo':
-        'https://pbs.twimg.com/profile_images/378800000593680476/aa0280b76541ed89ca802472ea174c57_400x400.png',
-    'url': 'https://twitter.com/katsummy',
-  },
-  {
-    'name': 'katsummy4',
-    // 'photo': Assets.photo.katsummy.path,
-    'photo':
-        'https://pbs.twimg.com/profile_images/378800000593680476/aa0280b76541ed89ca802472ea174c57_400x400.png',
-    'url': 'https://twitter.com/katsummy',
-  },
-  {
-    'name': 'katsummy5',
-    // 'photo': Assets.photo.katsummy.path,
-    'photo':
-        'https://pbs.twimg.com/profile_images/378800000593680476/aa0280b76541ed89ca802472ea174c57_400x400.png',
-    'url': 'https://twitter.com/katsummy',
-  },
-  {
-    'name': 'katsummy6',
-    // 'photo': Assets.photo.katsummy.path,
-    'photo':
-        'https://pbs.twimg.com/profile_images/378800000593680476/aa0280b76541ed89ca802472ea174c57_400x400.png',
-    'url': 'https://twitter.com/katsummy',
-  },
-  {
-    'name': 'katsummy7',
-    // 'photo': Assets.photo.katsummy.path,
-    'photo':
-        'https://pbs.twimg.com/profile_images/378800000593680476/aa0280b76541ed89ca802472ea174c57_400x400.png',
-    'url': 'https://twitter.com/katsummy',
-  },
-  {
-    'name': 'katsummy8',
+    'name': 'katsummy',
     // 'photo': Assets.photo.katsummy.path,
     'photo':
         'https://pbs.twimg.com/profile_images/378800000593680476/aa0280b76541ed89ca802472ea174c57_400x400.png',
