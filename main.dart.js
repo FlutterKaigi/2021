9f957a186c2834443d9f5e79910b351fa2868cf7
jiyuujin <jiyuujin@users.noreply.github.com>
2021-08-27 05:29:04 +0000

    Automated deployment: Fri Aug 27 05:29:04 UTC 2021 fcd5f58c2c1c3ca854812701b426322440349ef9

--- main.dart.js
+++ main.dart.js
@@ -67074,7 +67074,7 @@ $.yf=P.A(t.N,t._8)
 $.Sm=null
 $.acK=null
 $.aoJ=function(){var s=t.N
-return H.a([P.aD(["name","katsummy","photo","https://pbs.twimg.com/profile_images/378800000593680476/aa0280b76541ed89ca802472ea174c57_400x400.png","url","https://twitter.com/katsummy"],s,s)],t.SN)}()
+return H.a([P.aD(["name","jiyuujin","photo","https://pbs.twimg.com/profile_images/1154037739035119620/IX0vq03C_400x400.jpg","url","https://twitter.com/jiyuujinlab"],s,s),P.aD(["name","katsummy","photo","https://pbs.twimg.com/profile_images/378800000593680476/aa0280b76541ed89ca802472ea174c57_400x400.png","url","https://twitter.com/katsummy"],s,s)],t.SN)}()
 $.avp=H.a([],H.T("t<l<cc>(l<cc>)>"))
 $.fM=U.aBe()
 $.avt=U.aBf()
