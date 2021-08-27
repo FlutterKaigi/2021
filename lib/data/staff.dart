fcd5f58c2c1c3ca854812701b426322440349ef9
jiyuujin <jiyuujinunite@gmail.com>
2021-08-27 14:26:46 +0900

    add jiyuujin photo

--- lib/data/staff.dart
+++ lib/data/staff.dart
@@ -1,5 +1,11 @@
 final kStaffList = [
   {
+    'name': 'jiyuujin',
+    'photo':
+        'https://pbs.twimg.com/profile_images/1154037739035119620/IX0vq03C_400x400.jpg',
+    'url': 'https://twitter.com/jiyuujinlab',
+  },
+  {
     'name': 'katsummy',
     // 'photo': Assets.photo.katsummy.path,
     'photo':
