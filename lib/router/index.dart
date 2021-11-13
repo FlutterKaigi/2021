6109b60dedc621cd11d6aec249b95e1071dfea54
jiyuujin <jiyuujinunite@gmail.com>
2021-11-13 11:12:15 +0900

    feat: session page

--- lib/router/index.dart
+++ lib/router/index.dart
@@ -1,4 +1,5 @@
 import 'package:confwebsite2021/pages/index.dart';
+import 'package:confwebsite2021/pages/session.dart';
 import 'package:confwebsite2021/pages/staff.dart';
 import 'package:confwebsite2021/router/simple_route.dart';
 import 'package:flutter/material.dart';
@@ -9,6 +10,8 @@ Route<dynamic> buildRouters(RouteSettings settings) {
       return defaultRoute();
     case '/staff':
       return staffRoute();
+    case '/session':
+      return sessionRoute();
     default:
       return defaultRoute();
   }
@@ -16,10 +19,19 @@ Route<dynamic> buildRouters(RouteSettings settings) {
 
 SimpleRoute defaultRoute() {
   return SimpleRoute(
-      name: '/', title: 'FlutterKaigi 2021', builder: (context) => const TopPage());
+      name: '/',
+      title: 'FlutterKaigi 2021',
+      builder: (context) => const TopPage());
 }
 
 SimpleRoute staffRoute() {
   return SimpleRoute(
       name: '/staff', title: 'Staff', builder: (context) => const StaffPage());
+}
+
+SimpleRoute sessionRoute() {
+  return SimpleRoute(
+      name: '/session',
+      title: 'Session',
+      builder: (context) => const SessionPage());
 }
