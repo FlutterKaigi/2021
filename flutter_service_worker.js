5bcdd76560b5583a4602f82351d9f63cda1a86a2
jiyuujin <jiyuujin@users.noreply.github.com>
2021-11-28 07:37:58 +0000

    Automated deployment: Sun Nov 28 07:37:58 UTC 2021 cac999170202f55c4cb9c12c8b5520edb107eb9a

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "ed735b5ea5734c34ba82aaf382dc656a",
 "/": "ed735b5ea5734c34ba82aaf382dc656a",
-"main.dart.js": "c6a48384b6f274c25f45cd30e5bb779d",
+"main.dart.js": "25044c0f832602de62a4e4d92a7d9148",
 "icons/Icon-192.png": "02861f4b4a5637703ea4c586181ad448",
 "icons/Icon-512.png": "3f4f8b2bd2c99c17e609b03d55d09e14",
 "version.json": "a57d15030372c307522da8043a9c566c",
